cf4fadf305ca264fb145fa9bd389d1ff6a571f7d
JungHun98 <j7papa@naver.com>
2025-02-23 19:06:34 +0900

    comment: middleware.ts 불필요한 주석 제거

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,10 +11,10 @@ export const middleware = (req: NextRequest) => {
     return NextResponse.redirect(new URL('/not-found', req.url)); // 현재 경로가 /home이 아니면서 유효하지 않으면 404 페이지로 리디렉트
   }
 
-  return NextResponse.next(); // 허용된 값이면 정상 진행
+  return NextResponse.next();
 };
 
-// 미들웨어가 적용될 경로 설정 /home 이라 경로에서만 실행
+// 미들웨어가 적용될 경로 설정
 export const config = {
   matcher: ['/home/:hall*'],
 };
