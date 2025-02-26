460cba49dcc77605c427d224d6ef0b6c7319bf1d
JungHun98 <j7papa@naver.com>
2025-02-26 10:18:32 +0900

    refactor: Hall 이름 상수 값 변경

--- src/app/home/constants/hallName.ts
+++ src/app/home/constants/hallName.ts
@@ -1,6 +1,6 @@
 export const HALL_NAME = {
-  KSPO_DOME: 'olim',
-  JAMSIL_ARENA: 'jamsil',
+  KSPO_DOME: 'kspo-dome',
+  JAMSIL_ARENA: 'jamsil-arena',
 };
 
 export const ALL_HALL_NAME = Object.values(HALL_NAME);
