fd2c129f0fa3f14587230dd72e40319e8a31387a
jinsil <jinsil378900@gmail.com>
2025-02-27 13:37:45 +0900

    feat: 스플래시 화면에서 home 페이지 프리페칭

--- src/app/splash/page.tsx
+++ src/app/splash/page.tsx
@@ -9,9 +9,12 @@ const SplashPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    setTimeout(() => {
+    router.prefetch('/home');
+    const timeout = setTimeout(() => {
       router.replace('/home');
     }, 2000);
+
+    return () => clearTimeout(timeout);
   }, []);
 
   return (
