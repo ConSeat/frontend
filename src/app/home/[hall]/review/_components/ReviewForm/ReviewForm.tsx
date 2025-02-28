72ff6d0f1cec3142500704c09049f1dd6069c82a
JungHun98 <j7papa@naver.com>
2025-02-28 14:20:07 +0900

    feat: 리뷰 Form 컴포넌트 구현

---
+++ src/app/home/[hall]/review/_components/ReviewForm/ReviewForm.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import styles from './ReviewForm.module.scss';
+import { ActionDispatch, useRef } from 'react';
+import { ReviewStep } from '@/constants/review';
+import type { ReviewAction, ReviewData } from '@/types/review';
+
+interface ReviewFormProps {
+  reviewData: ReviewData;
+  dispatch: ActionDispatch<[action: ReviewAction]>;
+}
+
+const ReviewForm = ({ reviewData, dispatch }: ReviewFormProps) => {
+  const {
+    concert,
+    seatInfo,
+    additionalInfo,
+    images,
+    reviewSummary,
+    viewBlockInfo,
+    review,
+    currentStep,
+  } = reviewData;
+
+  const stepRef = useRef<number>(0);
+  stepRef.current = stepRef.current < currentStep ? currentStep : stepRef.current;
+
+  const isRender = (step: number) => stepRef.current >= step;
+
+  return (
+    <div className={styles.reviewFormLayout}>
+      <form>
+        {isRender(ReviewStep.ConcertSelect) && (
+          <div className={styles.reviewBanner}>
+            <h1>콘서트 선택</h1>
+          </div>
+        )}
+        {isRender(ReviewStep.SeatInfoSelect) && (
+          <div className={styles.reviewBanner}>
+            <h1>좌석 선택</h1>
+          </div>
+        )}
+        {isRender(ReviewStep.AdditionalInfoSelect) && (
+          <div className={styles.reviewBanner}>
+            <h1>추가 좌성 정보 선택</h1>
+          </div>
+        )}
+        {isRender(ReviewStep.ImageUpload) && (
+          <div className={styles.reviewBanner}>
+            <h1>시야 사진 등록</h1>
+          </div>
+        )}
+        {isRender(ReviewStep.SummaryInfoSelect) && (
+          <div className={styles.reviewBanner}>
+            <h1>시야 요약 정보</h1>
+          </div>
+        )}
+        {isRender(ReviewStep.viewBlockSelect) && (
+          <div className={styles.reviewBanner}>
+            <h1>시야 방해 요소</h1>
+          </div>
+        )}
+        {isRender(ReviewStep.reviewInput) && (
+          <div className={styles.reviewBanner}>
+            <h1>리뷰 작성</h1>
+          </div>
+        )}
+        {isRender(ReviewStep.submit) && (
+          <div className={styles.reviewBanner}>
+            <h1>리뷰 제출</h1>
+          </div>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default ReviewForm;
