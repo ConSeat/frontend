47a1afa40a4ab5adab294003d2790a71cc0afd57
jinsil <jinsil378900@gmail.com>
2025-03-03 19:53:26 +0900

    feat: textarea 생성

--- src/app/home/[hall]/review/page.tsx
+++ src/app/home/[hall]/review/page.tsx
@@ -5,10 +5,12 @@ import ReviewDropdownInput from './_components/ReviewDropdownInput/ReviewDropdow
 import { useState } from 'react';
 import Button from '@/components/Button/Button';
 import ButtonContainer from '@/components/ButtonContainer/ButtonContainer';
+import Textarea from '@/components/Textarea/Textarea';
 
 const ReviewPage = () => {
   const [selected, setSelected] = useState<string>('');
   const [selected2, setSelected2] = useState<string>('');
+  const [text, setText] = useState<string>('');
 
   return (
     <div style={{ width: '100%', height: '50vh', backgroundColor: 'white' }}>
@@ -20,6 +22,7 @@ const ReviewPage = () => {
         options={['FLOOR', '1층', '2층']}
         placeholder="층을 선택해주세요"
       />
+
       <ReviewDropdownInput
         value={selected2}
         onChange={(value) => {
@@ -34,6 +37,15 @@ const ReviewPage = () => {
         ]}
         placeholder="층을 선택해주세요"
       />
+
+      <Textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        maxLength={500}
+        placeholder="추가적으로 좋았던 점, 아쉬운 점, 관람팁 등을자유롭게 작성해주세요. 구체적으로 작성할 수록 다른 사람들에게 도움이 돼요!"
+        rows={5}
+      />
+
       <ButtonContainer>
         <>
           <Button variant="secondary">이전</Button>
