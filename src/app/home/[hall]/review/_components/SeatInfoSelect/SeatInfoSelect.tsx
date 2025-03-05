abf5a9e58163332fe67b43fc91c8e2688078de83
jinsil <jinsil378900@gmail.com>
2025-03-05 21:12:08 +0900

    fix: 오타 수정 및 불필요한 코드 제거

--- src/app/home/[hall]/review/_components/SeatInfoSelect/SeatInfoSelect.tsx
+++ src/app/home/[hall]/review/_components/SeatInfoSelect/SeatInfoSelect.tsx
@@ -35,8 +35,8 @@ const SeatInfoSelect = ({ data, dispatch }: SeatInfoSelectProps) => {
           onChange={(value) => {
             handleSeatInfoSelect(value, 'section');
           }}
-          options={['1구열', '2구역', '3구역', '4구역', '5구역', '6구역']}
-          placeholder="층을 선택해주세요"
+          options={['1구역', '2구역', '3구역', '4구역', '5구역', '6구역']}
+          placeholder="구역을 선택해주세요"
         />
       )}
       {data.floor !== FLOOR && data.section && (
@@ -46,7 +46,7 @@ const SeatInfoSelect = ({ data, dispatch }: SeatInfoSelectProps) => {
             handleSeatInfoSelect(value, 'column');
           }}
           options={['1열 ~ 6열', '6열 ~ 11열', '12열 ~ 15열', '16열 ~ 22열']}
-          placeholder="층을 선택해주세요"
+          placeholder="열을 선택해주세요"
         />
       )}
     </>
--- src/utils/toggleSetItem.ts
+++ src/utils/toggleSetItem.ts
@@ -1,4 +1,4 @@
-export const toggleSetItem = <T extends string>(set: Set<T>, item: T): Set<T> => {
+export const toggleSetItem = <T>(set: Set<T>, item: T): Set<T> => {
   const newSet = new Set<T>(set);
 
   if (newSet.has(item)) {
