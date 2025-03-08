07d12f04cc2294b5da947fa92c8dfa7910b3d810
JungHun98 <j7papa@naver.com>
2025-03-08 20:10:35 +0900

    refactor: Popup 컴포넌트 분리

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,30 +1,40 @@
 import styles from './Popup.module.scss';
+import { ReactNode } from 'react';
 import Button from '@/components/Button/Button';
 
-interface Popup {
-  subtitle: string;
-  bodyText: string;
-  onClose: () => void;
-  onClick: () => void;
-}
+const Popup = ({ children }: { children: ReactNode }) => {
+  return <div className={styles.popupLayout}>{children}</div>;
+};
 
-const Popup = ({ subtitle, bodyText, onClose, onClick }: Popup) => {
+const PopupTitle = ({ title, subtitle }: { title?: string; subtitle?: string }) => {
   return (
-    <div className={styles.popupLayout}>
-      <div className={styles.popupTextBox}>
-        <div className={styles.subtitle}>{subtitle}</div>
-        <div className={styles.bodyText}>{bodyText}</div>
-      </div>
-      <div className={styles.buttonBox}>
-        <Button className={styles.noButton} onClick={onClose}>
-          아니요
-        </Button>
-        <Button className={styles.yesButton} onClick={onClick}>
-          네
-        </Button>
-      </div>
+    <div className={styles.popupTitleWrapper}>
+      {title && <h2 className={styles.popupTitle}>{title}</h2>}
+      {subtitle && <p className={styles.popupSubtitle}>{subtitle}</p>}
     </div>
   );
 };
+
+const PopupButtonArea = ({
+  onClose,
+  onConfirm,
+}: {
+  onClose: () => void;
+  onConfirm: () => void;
+}) => {
+  return (
+    <div className={styles.buttonBox}>
+      <Button className={styles.noButton} onClick={onClose}>
+        아니요
+      </Button>
+      <Button className={styles.yesButton} onClick={onConfirm}>
+        네
+      </Button>
+    </div>
+  );
+};
+
+Popup.Title = PopupTitle;
+Popup.ButtonArea = PopupButtonArea;
 
 export default Popup;
