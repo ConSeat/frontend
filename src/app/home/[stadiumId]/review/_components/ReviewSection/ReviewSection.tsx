fa851bf6c878a57de9f5f56a8483fa1d347f6d1b
jinsil <jinsil378900@gmail.com>
2025-03-08 22:41:23 +0900

    fix: 합성 컴포넌트 memo 오류 해결

--- src/app/home/[stadiumId]/review/_components/ReviewSection/ReviewSection.tsx
+++ src/app/home/[stadiumId]/review/_components/ReviewSection/ReviewSection.tsx
@@ -1,22 +1,23 @@
 import styles from './ReviewSection.module.scss';
 import React, { ReactNode } from 'react';
 
-const ReviewSectionComponent = React.memo(({ children }: { children: ReactNode }) => {
+const ReviewSection = ({ children }: { children: ReactNode }) => {
   return <section className={styles.reviewSection}>{children}</section>;
-});
+};
 
-const Title = React.memo(({ title, subtitle }: { title?: string; subtitle?: string }) => {
-  return (
-    <div className={styles.reviewSectionTitleWrapper}>
-      {title && <h2 className={styles.reviewSectionTitle}>{title}</h2>}
-      {subtitle && <p className={styles.reviewSectionSubtitle}>{subtitle}</p>}
-    </div>
-  );
-});
+const ReviewSectionTitle = React.memo(
+  ({ title, subtitle }: { title?: string; subtitle?: string }) => {
+    return (
+      <div className={styles.reviewSectionTitleWrapper}>
+        {title && <h2 className={styles.reviewSectionTitle}>{title}</h2>}
+        {subtitle && <p className={styles.reviewSectionSubtitle}>{subtitle}</p>}
+      </div>
+    );
+  },
+);
 
-ReviewSectionComponent.displayName = 'ReviewSection';
-Title.displayName = 'ReviewSectionTitle';
+ReviewSectionTitle.displayName = 'ReviewSectionTitle';
 
-const ReviewSection = Object.assign(ReviewSectionComponent, { Title });
+ReviewSection.Title = ReviewSectionTitle;
 
 export default ReviewSection;
