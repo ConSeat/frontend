7535f5cdc130995fc2750825686887956bb3ba33
jinsil <jinsil378900@gmail.com>
2025-03-08 22:45:23 +0900

    refactor: 불필요한 코드 제거

--- src/app/home/[stadiumId]/review/_components/ReviewContainer/ReviewContainer.tsx
+++ src/app/home/[stadiumId]/review/_components/ReviewContainer/ReviewContainer.tsx
@@ -127,7 +127,6 @@ interface ReviewContainerProps {
 
 const ReviewContainer = React.memo(({ stadiumId }: ReviewContainerProps) => {
   const [state, dispatch] = useReducer(reviewReducer, createInitReviewData(stadiumId));
-  console.log(state);
 
   return <ReviewForm reviewData={state} dispatch={dispatch} />;
 });
