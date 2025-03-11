5efb29c7ba2dc62e1b93d5882d3a615008813ec4
JungHun98 <j7papa@naver.com>
2025-03-11 20:02:57 +0900

    feat: stage 이미지 컴포넌트 개발

--- src/components/SmallStageView/SmallStageView.tsx
+++ src/components/SmallStageView/SmallStageView.tsx
@@ -1,22 +1,30 @@
+'use client';
+
 import Button from '../Button/Button';
 import styles from './SmallStageView.module.scss';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { ZoomIn } from '@/assets';
 
 interface SmallStageViewProps {
   hallName: string;
-  onClick: () => void;
 }
 
-const SmallStageView = ({ hallName, onClick }: SmallStageViewProps) => {
+const SmallStageView = ({ hallName }: SmallStageViewProps) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/home/${hallName}/review/modal`);
+  };
+
   return (
     <div className={styles.stageContainer}>
-      <Image src={`/seats/${hallName}.svg`} width={79} height={72.875} alt="" />
+      <Image src={`/seats/${hallName}.svg`} width={79} height={73} alt="" />
       <Button
         className={styles.zoomInButton}
         onClick={(e) => {
           e.preventDefault();
-          onClick();
+          handleClick();
         }}
       >
         <ZoomIn />
