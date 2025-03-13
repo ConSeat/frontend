1d2ff713fb61c5b6b57457a50adf4a010673b79e
JungHun98 <j7papa@naver.com>
2025-03-13 17:45:04 +0900

    feat: home 페이지 UI 구현

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,24 +1,62 @@
+import StadiumItem from './_components/StadiumItem';
+import styles from './home.module.scss';
 import Link from 'next/link';
-import Splitter from '@/components/Splitter/Splitter';
+import { DefaultProfile, IcChat, LargeO, QnA } from '@/assets';
 import { STADIUM_INFO } from '@/constants/stadium';
-import { stadiumDisplayName } from '@/utils/stadium';
 
 const HomePage = () => {
   return (
-    <div>
-      <ul>
-        {STADIUM_INFO.active.map(({ stadiumId }) => (
-          <li key={stadiumId}>
-            <Link href={`/home/${stadiumId}`}>{stadiumDisplayName(stadiumId)}</Link>
-          </li>
-        ))}
-        <Splitter color="sub-white" />
-        {STADIUM_INFO.inactive.map(({ stadiumId }) => (
-          <li key={stadiumId}>
-            <Link href={`/home/${stadiumId}`}>{stadiumDisplayName(stadiumId)}</Link>
-          </li>
-        ))}
-      </ul>
+    <div className={styles.homeLayout}>
+      <header className={styles.homeHeader}>
+        <Link href="/mypage">
+          <DefaultProfile />
+        </Link>
+        <Link href="#">
+          <QnA />
+        </Link>
+      </header>
+      <main className={styles.homeMain}>
+        <section className={styles.homeExplanation}>
+          <h2 className={styles.title}>
+            한눈에 비교하는 <span className={styles.highlightingText}>콘서트장 시야</span>
+            <br />
+            공연장을 선택해주세요
+          </h2>
+          <div className={styles.review}>
+            <IcChat />
+            후기 +{121}
+          </div>
+        </section>
+        <section className={styles.openStadium}>
+          <h3 className={styles.subtitle}>공연장</h3>
+          <ul className={styles.stadiumList}>
+            {STADIUM_INFO.active.map(({ stadiumId, image, name }) => (
+              <StadiumItem
+                key={stadiumId}
+                stadiumName={name}
+                isActive={true}
+                backgroundImageSrc={image}
+                href={`/home/${stadiumId}`}
+              />
+            ))}
+          </ul>
+        </section>
+        <section className={styles.comingSoonStadium}>
+          <h3 className={styles.subtitle}>오픈예정</h3>
+          <ul className={styles.stadiumList}>
+            {STADIUM_INFO.inactive.map(({ stadiumId, image, name }) => (
+              <StadiumItem
+                key={stadiumId}
+                stadiumName={name}
+                isActive={false}
+                backgroundImageSrc={image}
+                href="#"
+              />
+            ))}
+          </ul>
+        </section>
+      </main>
+      <LargeO className={styles.svgO} width={201} height={320} />
     </div>
   );
 };
