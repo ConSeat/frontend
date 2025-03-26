dfa28ff493fd49595b4aeefd652337b2d3385a0d
JungHun98 <j7papa@naver.com>
2025-03-26 19:02:08 +0900

    fix: sns 로고 겹침현상 해결 - Icon -> Image 컴포넌트로 변경

--- src/components/ShareArea/ShareArea.tsx
+++ src/components/ShareArea/ShareArea.tsx
@@ -1,6 +1,7 @@
 import Button from '../Button/Button';
 import Icon from '../Icon/Icon';
 import styles from './ShareArea.module.scss';
+import Image from 'next/image';
 
 interface ShareAreaProp {
   onCopy: () => void;
@@ -16,17 +17,17 @@ const ShareArea = ({ onCopy, onSave, onShareKakao, onShareTwitter, isLogin }: Sh
       <div className={styles.shareAreaText}>결과 공유하기</div>
       <div className={styles.buttonContainer}>
         <Button className={styles.linkCopy} onClick={onCopy}>
-          <Icon icon="LinkCopy" />
+          <Icon icon="LinkCopy" size={24} />
         </Button>
         <Button className={styles.kakao} onClick={onShareKakao}>
-          <img src="/logo/kakaotalk.svg" />
+          <Image src="/logo/kakaotalk.svg" width={28} height={28} alt="" />
         </Button>
         <Button className={styles.twitter} onClick={onShareTwitter}>
-          <img src="/logo/X.svg" width={20} height={20} />
+          <Image src="/logo/X.svg" width={20} height={20} alt="" />
         </Button>
         {isLogin && (
           <Button className={styles.save} onClick={onSave}>
-            <Icon icon="Bookmark" color="#E9ECEF" />
+            <Icon icon="Bookmark" color="#E9ECEF" size={24} />
           </Button>
         )}
       </div>
