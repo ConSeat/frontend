b2cf63004c68999ea2976dca91705535e1058b08
JungHun98 <j7papa@naver.com>
2025-03-26 16:47:23 +0900

    feat: 스와이프 이미지 전환 기능 추가

--- src/components/ImageSlide/ImageSlide.tsx
+++ src/components/ImageSlide/ImageSlide.tsx
@@ -3,6 +3,7 @@
 import Button from '../Button/Button';
 import Icon from '../Icon/Icon';
 import styles from './ImageSlide.module.scss';
+import { TouchEventHandler, useRef } from 'react';
 
 interface ImageSlideProps {
   imageSrcArray: string[];
@@ -13,15 +14,45 @@ interface ImageSlideProps {
 }
 
 const ImageSlide = ({ currentIndex, imageSrcArray, height, onPrev, onNext }: ImageSlideProps) => {
+  const imageRef = useRef<HTMLDivElement>(null);
+  const startXRef = useRef<number>(0);
+  const endXRef = useRef<number>(0);
   const totalLength = imageSrcArray.length;
 
+  const handleTouchStart: TouchEventHandler<HTMLDivElement> = (event) => {
+    startXRef.current = event.touches[0].clientX;
+  };
+
+  const handleTouchMove: TouchEventHandler<HTMLDivElement> = (event) => {
+    endXRef.current = event.touches[0].clientX;
+  };
+
+  const handleTouchEnd: TouchEventHandler<HTMLDivElement> = () => {
+    const diffX = startXRef.current - endXRef.current;
+
+    if (Math.abs(diffX) > 0) {
+      if (diffX < 0) {
+        onPrev();
+      } else {
+        onNext();
+      }
+    }
+  };
+
   return (
-    <div className={styles.slideContainer}>
+    <div
+      className={styles.slideContainer}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+      ref={imageRef}
+    >
       <div
         style={{
           width: `${100 * totalLength}%`,
           height: `${height}px`,
           transform: `translateX(-${(100 / totalLength) * (currentIndex - 1)}%)`,
+          transition: `transform 0.5s`,
         }}
       >
         {imageSrcArray.map((src, index) => {
