79023ca6b8538ac9fd44bfc759a5b9ef10707f7f
JungHun98 <j7papa@naver.com>
2025-04-06 17:24:34 +0900

    feat: mypage 사용자 정보 UI 구현

---
+++ src/app/mypage/_components/UserInfo/UserInfo.tsx
@@ -0,0 +1,33 @@
+import styles from './UserInfo.module.scss';
+import Image from 'next/image';
+import Button from '@/components/Button/Button';
+import Icon from '@/components/Icon/Icon';
+
+interface UserInfo {
+  thumbnail: string;
+  nickName: string;
+  email: string;
+}
+
+const UserInfo = ({ thumbnail, nickName, email }: UserInfo) => {
+  return (
+    <div className={styles.userInfoContainer}>
+      <div className={styles.profileContainer}>
+        <div className={styles.imageContainer}>
+          <Image src={thumbnail} width={64} height={64} alt="프로필사진" />
+        </div>
+        <Button className={styles.editButton}>
+          <Icon icon="Pencil" size={10} />
+        </Button>
+      </div>
+      <div className={styles.textProfile}>
+        <div className={styles.userName}>
+          <span className={styles.nickName}>{nickName}</span>님
+        </div>
+        <div className={styles.email}>{email}</div>
+      </div>
+    </div>
+  );
+};
+
+export default UserInfo;
