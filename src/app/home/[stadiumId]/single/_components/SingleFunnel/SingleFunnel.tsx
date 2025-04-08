9a5bf4159f1ed7f6420206e49897a30edea34dfe
jinsil <jinsil378900@gmail.com>
2025-04-08 21:13:24 +0900

    feat: SingleFunnel에 각 step 컴포넌트 추가

--- src/app/home/[stadiumId]/single/_components/SingleFunnel/SingleFunnel.tsx
+++ src/app/home/[stadiumId]/single/_components/SingleFunnel/SingleFunnel.tsx
@@ -1,62 +1,64 @@
 'use client';
 
-import styles from './SingleFunnel.module.scss';
+import ProgressBar from '../../../_components/ProgressBar/ProgressBar';
+import SingleResultStep from '../SingleResultStep/SingleResultStep';
+import SingleSeatingStep from '../SingleSeatingStep/SingleSeatingStep';
+import SingleSectionStep from '../SingleSectionStep/SingleSectionStep';
 import React from 'react';
 import useFunnel from '@/hooks/useFunnel';
-import Button from '@/components/Button/Button';
 import Spacing from '@/components/Spacing/Spacing';
 
 interface SingleFunnelProps {
   stadiumId: number;
 }
 
+type Step = 'Section' | 'Seating' | 'Result';
+
+interface SingleFunnelData {
+  sectionId: number;
+  seatingId: number;
+}
+
 const SingleFunnel = ({ stadiumId }: SingleFunnelProps) => {
-  const { Funnel, currentStep, setStep, data, setData } = useFunnel<
-    'Section' | 'Seating' | 'Result',
-    {
-      sectionId: number;
-      seatingId: number;
-    }
-  >('Section');
+  const { Funnel, step, setStep, data, setData } = useFunnel<Step, SingleFunnelData>({
+    initialStep: 'Section',
+    initialData: {
+      sectionId: 0,
+      seatingId: 0,
+    },
+  });
 
   return (
     <>
-      <div>네비게이션바 (stadiumId: {stadiumId})</div>
-      <Spacing size={32} />
-      <main className={styles.singleFunnelLayout}>
-        <Funnel currentStep={currentStep}>
-          <Funnel.Step name="Section">
-            <div>섹션 선택 영역</div>
-            <Button
-              onClick={() => {
-                setData((prev) => ({ ...prev, sectionId: 10 }));
-                setStep('Seating');
-              }}
-            >
-              좌석 선택으로
-            </Button>
-          </Funnel.Step>
+      <ProgressBar steps={['Section', 'Seating', 'Result']} currentStep={step} />
 
-          <Funnel.Step name="Seating">
-            <div>좌석 선택 영역</div>
-            <Button
-              onClick={() => {
-                setData((prev) => ({ ...prev, seatingId: 42 }));
-                setStep('Result');
-              }}
-            >
-              결과 보기로
-            </Button>
-          </Funnel.Step>
+      <Spacing size={45} />
 
-          <Funnel.Step name="Result">
-            <div>결과 페이지</div>
-            <p>선택된 섹션: {data.sectionId}</p>
-            <p>선택된 좌석: {data.seatingId}</p>
-            <Button onClick={() => setStep('Section')}>처음으로</Button>
-          </Funnel.Step>
-        </Funnel>
-      </main>
+      <Funnel currentStep={step}>
+        <Funnel.Step name="Section">
+          <SingleSectionStep
+            stadiumId={stadiumId}
+            step={step}
+            setStep={setStep}
+            data={data}
+            setData={setData}
+          />
+        </Funnel.Step>
+
+        <Funnel.Step name="Seating">
+          <SingleSeatingStep
+            stadiumId={stadiumId}
+            step={step}
+            setStep={setStep}
+            data={data}
+            setData={setData}
+          />
+        </Funnel.Step>
+
+        <Funnel.Step name="Result">
+          <SingleResultStep stadiumId={stadiumId} />
+        </Funnel.Step>
+      </Funnel>
     </>
   );
 };
