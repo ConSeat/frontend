e32f997ac7ff70bbb101ea76cf69c9b7f2438c8a
jinsil <jinsil378900@gmail.com>
2025-04-15 22:02:35 +0900

    feat: login, google 로그인 연동

--- src/app/signin/_components/SigninModal/SigninModal.tsx
+++ src/app/signin/_components/SigninModal/SigninModal.tsx
@@ -7,7 +7,7 @@ import Button from '@/components/Button/Button';
 import Icon from '@/components/Icon/Icon';
 import MainView from '@/components/MainView/MainView';
 import Modal from '@/components/Modal';
-import Spacing from '@/components/Spacing/Spacing';
+import { PUBLIC_ENV } from '@/config/env';
 
 const SigninModal = () => {
   const { closeModal } = useRouterModal({
@@ -22,23 +22,35 @@ const SigninModal = () => {
 
         <Modal.Header title="" onClose={() => closeModal()} />
 
-        <Spacing size={184} />
-
         <Icon icon="MainLogo" />
-
-        <Spacing size={241} />
 
         <div className={styles.signinContainer}>
           <div className={styles.buttonContainer}>
-            <Button className={styles.googleButton}>
+            <Button
+              className={styles.googleButton}
+              onClick={() => {
+                window.open(`${PUBLIC_ENV.baseUrl}/auth/login/google`, '_self');
+              }}
+            >
               <Image src="/logo/google.svg" alt="구글 아이콘" width={24} height={24} />
               Google로 로그인하기
             </Button>
-            <Button className={styles.kakaoButton}>
+            <Button
+              className={styles.kakaoButton}
+              onClick={() => {
+                window.open(`${PUBLIC_ENV.baseUrl}/auth/login/kakao`, '_self');
+              }}
+            >
               <Image src="/logo/kakaotalk.svg" alt="카카오톡 아이콘" width={28} height={28} />
               카카오톡 로그인하기
             </Button>
-            <Button className={styles.xButton}>
+            <Button
+              className={styles.xButton}
+              // TODO: x 로그인 API 연동 후 주석 해제 (서버 구현 안 됨)
+              // onClick={() => {
+              //   window.open(`${PUBLIC_ENV.baseUrl}/auth/login/twitter`, '_self');
+              // }}
+            >
               <Image src="/logo/X.svg" alt="x 아이콘" width={20} height={20} />
               X로 로그인하기
             </Button>
