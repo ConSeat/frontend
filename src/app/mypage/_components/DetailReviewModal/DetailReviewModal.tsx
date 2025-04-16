7955c7b7045bfa497620659094ffdda8b3a66c02
JungHun98 <j7papa@naver.com>
2025-04-16 21:42:52 +0900

    refactor: reviewCard props 변경 적용

--- src/app/mypage/_components/DetailReviewModal/DetailReviewModal.tsx
+++ src/app/mypage/_components/DetailReviewModal/DetailReviewModal.tsx
@@ -30,22 +30,18 @@ const DetailReviewModal = ({
         <div className={styles.reviewContainer}>
           <div className={styles.reviewDetail}>
             <ReviewCard
-              imageSrcArray={[
-                '/images/kspo-dome.jpg',
-                '/images/kspo-dome.jpg',
-                '/images/kspo-dome.jpg',
-              ]}
+              images={['/images/kspo-dome.jpg', '/images/kspo-dome.jpg', '/images/kspo-dome.jpg']}
               features={['돌돌출']}
               obstructions={['팬스방해가 있어요', '단차가 심해요']}
               concertName={'2025 SVT 9TH FAN MEETING 〈SEVEN TEEN in CARAT LAND〉'}
-              reviewDescription={
+              contents={
                 '본무대 안쪽에서 하는 무대는 잘 안보임. 근데 본무대랑 돌출 다 잘보여서 좋았음. 펜스 시야방해가 조금 있기는 한데 그래도 저는 괜찮았던 것 같아요. 제트플립 기준 0배로 확대한 사진입니다.'
               }
-              profileSrc={'/logo/google.svg'}
-              uploadTime={'2주전'}
-              userName={'호빵맨'}
+              writerSrc={'/logo/google.svg'}
+              createdAt={'2주전'}
+              writerNickname={'호빵맨'}
               likeNumber={0}
-              isSaved={false}
+              isBookmarked={false}
               isLiked={false}
               handleClickMore={() => {}}
               handleClickLike={() => {}}
