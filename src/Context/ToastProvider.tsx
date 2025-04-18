1f038631c182d013f8e5c45a35466f9e9e24b511
JungHun98 <j7papa@naver.com>
2025-04-18 17:16:35 +0900

    feat: toastProvider 구현

---
+++ src/Context/ToastProvider.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { ReactNode, createContext, useContext, useState } from 'react';
+import Portal from '@/components/Portal';
+import Toast, { ToastType } from '@/components/Toast/Toast';
+
+interface ToastContextType {
+  activateToast: (message: string, type: ToastType) => void;
+}
+
+const ToastContext = createContext<ToastContextType | undefined>(undefined);
+
+export const ToastProvider = ({ children }: { children: ReactNode }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [message, setMessage] = useState('');
+  const [type, setType] = useState<ToastType>('default');
+
+  const activateToast = (message: string, type: ToastType) => {
+    setIsOpen(true);
+    setMessage(message);
+    setType(type);
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <ToastContext.Provider value={{ activateToast }}>
+      <Portal isOpen={isOpen}>
+        <Toast type={type} text={message} onClose={onClose} />
+      </Portal>
+      {children}
+    </ToastContext.Provider>
+  );
+};
+
+export const useToast = () => {
+  return useContext(ToastContext);
+};
