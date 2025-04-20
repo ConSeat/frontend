c3b4c9f224d6db40c8e6f95da1fb49331a2101ba
jinsil <jinsil378900@gmail.com>
2025-04-20 19:31:32 +0900

    feat: 로딩 중일 때 option 사라지는 UX 해결

--- src/app/home/[stadiumId]/review/_components/ConcertSelect/ConcertSelect.tsx
+++ src/app/home/[stadiumId]/review/_components/ConcertSelect/ConcertSelect.tsx
@@ -3,7 +3,7 @@
 import { NONE_SELECT } from '../../_constants/info';
 import { REVIEW } from '../../_constants/review';
 import ReviewDropdownInput from '../ReviewDropdownInput/ReviewDropdownInput';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useDebounce from '@/hooks/common/useDebounce';
 import { useFetchStadiumConcerts } from '@/hooks/queries/useFetchStadium';
 import type { StadiumConcertInfo } from '@/apis/stadium/stadium.api';
@@ -17,10 +17,17 @@ interface ConcertSelectProps {
 const ConcertSelect = ({ stadiumId, dispatch }: ConcertSelectProps) => {
   const [inputValue, setInputValue] = useState<string>('');
   const [selectedId, setSelectedId] = useState<number>(NONE_SELECT);
+  const [cachedConcerts, setCachedConcerts] = useState<StadiumConcertInfo[]>([]);
 
   const debouncedQuery = useDebounce(inputValue, 300);
   const { data: stadiumConcerts } = useFetchStadiumConcerts(stadiumId, debouncedQuery);
-  const concerts = stadiumConcerts?.data.concerts ?? [];
+  const fetchedConcerts = stadiumConcerts?.data.concerts ?? [];
+
+  useEffect(() => {
+    if (fetchedConcerts.length > 0) {
+      setCachedConcerts(fetchedConcerts);
+    }
+  }, [fetchedConcerts]);
 
   const handleInputChange = (value: string) => {
     setInputValue(value);
@@ -41,7 +48,7 @@ const ConcertSelect = ({ stadiumId, dispatch }: ConcertSelectProps) => {
       value={inputValue}
       onChange={handleSelect}
       onInputChange={handleInputChange}
-      options={concerts}
+      options={cachedConcerts}
       selectedId={selectedId}
       placeholder="콘서트명을 검색해주세요"
     />
