e81d6f67ddc5d52ff926478719812e899f6b5013
JungHun98 <j7papa@naver.com>
2025-04-21 11:52:33 +0900

    feat: useAuth 훅 구현

---
+++ src/hooks/common/useAuth.ts
@@ -0,0 +1,34 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { usePopup } from '@/providers/PopupProvider';
+import { requireLogin } from '@/utils/requireLogin';
+
+export const useAuth = () => {
+  const { showPopup } = usePopup();
+  const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const checkAndExecute = async (action: () => void, popupText: string) => {
+    if (!isClient) return;
+
+    try {
+      await requireLogin(action);
+    } catch (error) {
+      if (error instanceof Error && error.message === 'LoginRequired') {
+        showPopup({
+          title: '로그인 필요',
+          subtitle: popupText,
+          onConfirm: () => router.push('/signin'),
+        });
+      }
+    }
+  };
+
+  return { checkAndExecute };
+};
