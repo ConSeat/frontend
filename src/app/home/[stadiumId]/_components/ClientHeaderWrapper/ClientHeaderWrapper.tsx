b366aaa2d706b28def179bd0cffaae3061881ad4
jinsil <jinsil378900@gmail.com>
2025-04-23 21:32:20 +0900

    refactor: 모든 페이지 Header 컴포넌트 적용

--- src/app/home/[stadiumId]/_components/ClientHeaderWrapper/ClientHeaderWrapper.tsx
+++ src/app/home/[stadiumId]/_components/ClientHeaderWrapper/ClientHeaderWrapper.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import Header from '@/components/Header/Header';
+import Icon from '@/components/Icon/Icon';
 import type { StadiumInfo } from '@/types/stadium';
 
 interface Props {
@@ -9,13 +10,19 @@ interface Props {
 }
 
 const ClientHeaderWrapper = ({ stadium }: Props) => {
+  const router = useRouter();
   const pathname = usePathname();
 
   const isHeaderHidden = pathname.includes('/review/complete');
 
   if (isHeaderHidden) return null;
 
-  return <Header stadium={stadium} />;
+  return (
+    <Header
+      left={<Icon icon="LeftArrow" onClick={() => router.back()} />}
+      title={stadium.stadiumName}
+    />
+  );
 };
 
 export default ClientHeaderWrapper;
