db0dc59e9e71571ca8d807906ea8e39bb31a4f52
JungHun98 <j7papa@naver.com>
2025-04-23 20:42:32 +0900

    feat: 리뷰 컨테이너 구현

---
+++ src/app/home/[stadiumId]/single/[seatingId]/_components/SingleResult/SingleResult.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import ReviewCardList from '../ReviewCardList';
+import styles from './SingleResult.module.scss';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { useFetchSeating } from '@/hooks/queries/useFetchSeatingReview';
+import Button from '@/components/Button/Button';
+import ButtonContainer from '@/components/ButtonContainer/ButtonContainer';
+import Highlight from '@/components/Highlight/Highlight';
+import PageExplanation from '@/components/PageExplanation';
+import ShareArea from '@/components/ShareArea';
+import Spacing from '@/components/Spacing/Spacing';
+
+const SingleResult = ({ stadiumId }) => {
+  const { data } = useFetchSeating(stadiumId);
+  const router = useRouter();
+
+  if (!data) return null;
+
+  console.log(data);
+
+  const handleCopyLink = () => {
+    navigator.clipboard.writeText(window.location.href);
+    alert('링크가 복사되었습니다!');
+  };
+
+  return (
+    <div className={styles.singleResultStepLayout}>
+      <div>
+        <PageExplanation>
+          <PageExplanation.Title>
+            <Highlight variant="background">
+              {data.floorName || data.sectionName} {data.sectionName || data.seatingName}
+            </Highlight>
+            은
+            <br />
+            본무대, 돌출, 전광판 모두 잘보여요
+          </PageExplanation.Title>
+        </PageExplanation>
+
+        <Spacing size={52} />
+        <ReviewCardList reviews={data.reviews} />
+        <ShareArea
+          onCopy={handleCopyLink}
+          onShareKakao={() => {}}
+          onShareTwitter={() => {}}
+          onSave={() => {}}
+          isLogin={true}
+        />
+      </div>
+      <ButtonContainer>
+        <Button onClick={() => router.push(`/home/${stadiumId}`)}>검색 완료</Button>
+      </ButtonContainer>
+    </div>
+  );
+};
+
+export default SingleResult;
