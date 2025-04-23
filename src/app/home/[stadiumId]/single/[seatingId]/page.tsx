aed72aaa207d4cb5333c1602b347743efb3a13c4
JungHun98 <j7papa@naver.com>
2025-04-23 20:42:52 +0900

    chore: 결과 페이지에 개발 내용 적용

--- src/app/home/[stadiumId]/single/[seatingId]/page.tsx
+++ src/app/home/[stadiumId]/single/[seatingId]/page.tsx
@@ -1,13 +1,19 @@
 import ProgressBar from '../../_components/ProgressBar/ProgressBar';
 import { SINGLE_FUNNEL_STEPS } from '../_constants/funnelSteps';
 import SingleResult from './_components/SingleResult';
+import { HydrationBoundary } from '@tanstack/react-query';
 import Spacing from '@/components/Spacing/Spacing';
+import { seatingReviewQueries } from '@/apis/review/seating.query';
+import { createPrefetchedQueryClient } from '@/utils/createPrefetchedQueryClient';
 
 const ResultPage = async ({ params }) => {
   const { stadiumId } = await params;
   const { seatingId } = await params;
-  console.log(stadiumId, seatingId);
-  if (!seatingId) {
+  const { dehydratedState } = await createPrefetchedQueryClient([
+    seatingReviewQueries.seating(seatingId),
+  ]);
+
+  if (!stadiumId || !seatingId) {
     return <p>잘못된 접근입니다.</p>;
   }
 
@@ -16,8 +22,9 @@ const ResultPage = async ({ params }) => {
       <ProgressBar steps={SINGLE_FUNNEL_STEPS} currentStep="Result" />
 
       <Spacing size={45} />
-
-      <SingleResult stadiumId={stadiumId} data={{ seatingId }} />
+      <HydrationBoundary state={dehydratedState}>
+        <SingleResult stadiumId={stadiumId} />
+      </HydrationBoundary>
     </>
   );
 };
