e6f0ef8de4b9ea89070eb3c59238e98db25a5922
JungHun98 <j7papa@naver.com>
2025-04-24 13:19:43 +0900

    feat: 하단 버튼 기능 수정

--- src/app/home/[stadiumId]/single/[seatingId]/_components/SearchEndButton/SearchEndButton.tsx
+++ src/app/home/[stadiumId]/single/[seatingId]/_components/SearchEndButton/SearchEndButton.tsx
@@ -3,34 +3,27 @@
 import { useRouter } from 'next/navigation';
 import Button from '@/components/Button/Button';
 import ButtonContainer from '@/components/ButtonContainer/ButtonContainer';
-import { usePopup } from '@/providers/PopupProvider';
 
 interface SearchEndButtonProps {
   stadiumId: number;
 }
 const SearchEndButton = ({ stadiumId }: SearchEndButtonProps) => {
   const router = useRouter();
-  const { showPopup } = usePopup();
 
-  const handleClickConfirm = () => {
+  const handleSearchEnd = () => {
+    router.push(`/home`);
+  };
+
+  const handleSearchRe = () => {
     router.push(`/home/${stadiumId}`);
   };
 
   return (
     <ButtonContainer>
-      <Button
-        onClick={() =>
-          showPopup({
-            onConfirm: handleClickConfirm,
-            title: '검색을 종료하시겠어요?',
-            subtitle: '자리 검색을 종료하고 싶다면, 종료버튼을 눌러주세요',
-            cancelText: '다시 검색하기',
-            confirmText: '종료',
-          })
-        }
-      >
-        검색 완료
+      <Button variant="secondary" onClick={handleSearchEnd}>
+        검색 종료
       </Button>
+      <Button onClick={handleSearchRe}>다시 검색하기</Button>
     </ButtonContainer>
   );
 };
