51902b33dfe490e1b17bf04c4ac0f1fff0186187
jinsil <jinsil378900@gmail.com>
2025-04-24 13:35:42 +0900

    feat: 리듀서로 filter 관리하기

---
+++ src/app/home/[stadiumId]/single/[seatingId]/detail/_components/ReviewDetailContainer/ReviewDetailContainer.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import ReviewDetailContent from '../ReviewDetailContent/ReviewDetailContent';
+import React, { useReducer } from 'react';
+import { toggleItem } from '@/utils/toggleItem';
+
+export interface FilterState {
+  stadiumId: number;
+  seatingId: number;
+  features: number[];
+  obstructions: number[];
+  lastReviewId: number;
+  sort: string;
+}
+
+export type FilterAction =
+  | { type: 'SEATING'; payload: { seatingId: number } }
+  | { type: 'FEATURES'; payload: { feature: number } }
+  | { type: 'OBSTRUCTIONS'; payload: { obstruction: number } }
+  | { type: 'LAST_REVIEW_ID'; payload: { lastReviewId: number } }
+  | { type: 'SORT'; payload: { sort: string } };
+
+const createInitFilterData = (stadiumId: number, seatingId: number) => {
+  const initData = {
+    stadiumId,
+    seatingId,
+    features: [],
+    obstructions: [],
+    lastReviewId: 0,
+    sort: '',
+  };
+
+  return initData;
+};
+
+export interface ReviewDetailContainerProps {
+  stadiumId: number;
+  seatingId: number;
+}
+
+export interface ReviewSummary {
+  floorName: string;
+  sectionName: string;
+  seatingName: string;
+}
+
+const updateState = (state: FilterState, updates: Partial<FilterState>): FilterState => ({
+  ...state,
+  ...updates,
+});
+
+const filterReducer = (state: FilterState, action: FilterAction): FilterState => {
+  switch (action.type) {
+    case 'SEATING':
+      return updateState(state, {
+        seatingId: action.payload.seatingId,
+      });
+
+    case 'FEATURES':
+      return updateState(state, {
+        features: toggleItem(state.features, action.payload.feature),
+      });
+
+    case 'OBSTRUCTIONS':
+      return updateState(state, {
+        obstructions: toggleItem(state.obstructions, action.payload.obstruction),
+      });
+
+    case 'LAST_REVIEW_ID':
+      return updateState(state, { lastReviewId: action.payload.lastReviewId });
+
+    case 'SORT':
+      return updateState(state, { sort: action.payload.sort });
+
+    default:
+      return state;
+  }
+};
+
+const ReviewDetailContainer = ({ stadiumId, seatingId }: ReviewDetailContainerProps) => {
+  const [state, dispatch] = useReducer(filterReducer, createInitFilterData(stadiumId, seatingId));
+
+  return (
+    <ReviewDetailContent
+      filterData={state}
+      dispatch={dispatch}
+      stadiumId={stadiumId}
+      seatingId={seatingId}
+    />
+  );
+};
+
+export default ReviewDetailContainer;
--- src/app/home/[stadiumId]/single/[seatingId]/detail/page.tsx
+++ src/app/home/[stadiumId]/single/[seatingId]/detail/page.tsx
@@ -1,7 +1,18 @@
+import ReviewDetailContainer from './_components/ReviewDetailContainer/ReviewDetailContainer';
+import ReviewDetailHeader from './_components/ReviewDetailHeader/ReviewDetailHeader';
 import React from 'react';
+import Splitter from '@/components/Splitter/Splitter';
 
-const SingleDetailPage = () => {
-  return <div>SingleDetailPage</div>;
+const SingleDetailPage = async ({ params }) => {
+  const { stadiumId, seatingId } = await params;
+
+  return (
+    <div>
+      <ReviewDetailHeader />
+      <Splitter color="sub-gray8" />
+      <ReviewDetailContainer stadiumId={stadiumId} seatingId={seatingId} />
+    </div>
+  );
 };
 
 export default SingleDetailPage;
