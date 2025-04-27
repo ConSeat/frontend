b6fae88f1d7ccd3e3ed3176ce264a3617e3e31f0
jinsil <jinsil378900@gmail.com>
2025-04-27 17:02:39 +0900

    feat: useFetchMyReview 무한 스크롤 코드 추가

--- src/app/mypage/_components/ReviewCollection/ReviewCollection.tsx
+++ src/app/mypage/_components/ReviewCollection/ReviewCollection.tsx
@@ -22,8 +22,8 @@ interface ReviewCollectionProps {
   reviewNumber: number;
   stadiums: Stadiums[];
   useFetchReview:
-    | ((stadiumId: number) => UseQueryResult<MyReviewResponse, Error>)
-    | ((stadiumId: number) => UseQueryResult<MyBookmarkResponse, Error>);
+    | (({ stadiumId }) => UseQueryResult<MyReviewResponse, Error>)
+    | (({ stadiumId }) => UseQueryResult<MyBookmarkResponse, Error>);
 }
 
 interface ReviewStatusTagProps {
@@ -35,8 +35,8 @@ interface ReviewListProps {
   stadium: string;
   onClick: (reviewId: number, status?: string) => void;
   useFetchReview:
-    | ((stadiumId: number) => UseQueryResult<MyReviewResponse, Error>)
-    | ((stadiumId: number) => UseQueryResult<MyBookmarkResponse, Error>);
+    | (({ stadiumId }) => UseQueryResult<MyReviewResponse, Error>)
+    | (({ stadiumId }) => UseQueryResult<MyBookmarkResponse, Error>);
 }
 
 const ReviewStatusTag = ({ status }: ReviewStatusTagProps) => {
@@ -48,7 +48,7 @@ const ReviewList = ({ stadium, stadiumId, onClick, useFetchReview }: ReviewListP
     notFound();
   }
 
-  const { data, isLoading } = useFetchReview(stadiumId);
+  const { data, isLoading } = useFetchReview({ stadiumId });
 
   if (isLoading) return <LoadingSpinner />;
 
