028de446381c558f6abacc1e5df3ab79be4923e3
JungHun98 <j7papa@naver.com>
2025-05-03 17:26:53 +0900

    fix: 빌드에러 수정 warning->Warning

--- src/components/ErrorCapture/ErrorCapture.tsx
+++ src/components/ErrorCapture/ErrorCapture.tsx
@@ -11,7 +11,7 @@ const ErrorCapture = () => {
   useEffect(() => {
     if (!error) return;
 
-    activateToast(error.message, 'warning');
+    activateToast(error.message, 'Warning');
   }, [error]);
 
   return <></>;
