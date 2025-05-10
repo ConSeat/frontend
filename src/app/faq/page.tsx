c866ea20c83717514c706b3fd3a49d180db604b7
jinsil <jinsil378900@gmail.com>
2025-05-11 03:22:58 +0900

    feat: 모든 섹션 추가

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -4,6 +4,7 @@ import FAQSection from './_components/FAQSection/FAQSection';
 import styles from './page.module.scss';
 import React from 'react';
 import Spacing from '@/components/Spacing/Spacing';
+import Splitter from '@/components/Splitter/Splitter';
 
 const FAQPage = () => {
   return (
@@ -19,6 +20,19 @@ const FAQPage = () => {
             content="A. 아직 해당 구역에 등록된 후기가 없는 경우, 검색 결과가 표시되지 않을 수 있습니다. 다른 구역을 선택해보시거나, 직접 후기를 등록해 주시면 다른 사용자에게도 도움이 될 거예요."
           />
         </FAQSection>
+
+        <FAQSection title="후기등록">
+          <FAQDropdown title="🗑️ 직접 등록한 후기를 삭제하고 싶어요" content="답변" />
+          <FAQDropdown title="🤔 등록한 후기가 반려되었어요" content="답변" />
+        </FAQSection>
+
+        <Splitter />
+
+        <FAQSection title="관리">
+          <FAQDropdown title="🙇🏻‍♀️ 회원탈퇴를 진행하고 싶어요" content="답변" />
+        </FAQSection>
+
+        <Splitter />
       </div>
     </>
   );
