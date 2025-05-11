86cea065cfe51e216f79555a32b6961dd11a68a5
jinsil <jinsil378900@gmail.com>
2025-05-12 02:13:52 +0900

    feat: 문의하기 페이지에서 팝업으로 변경

--- src/app/faq/_components/ContactSection/ContactSection.tsx
+++ src/app/faq/_components/ContactSection/ContactSection.tsx
@@ -1,18 +1,32 @@
 'use client';
 
 import styles from './ContactSection.module.scss';
-import { useRouter } from 'next/navigation';
 import React from 'react';
 import Button from '@/components/Button/Button';
+import { usePopup } from '@/providers/PopupProvider';
 
 const ContactSection = () => {
-  const router = useRouter();
+  const { showPopup } = usePopup();
+
+  const handleContactButton = () => {
+    showPopup({
+      title: '📮 문의하기',
+      subtitle: '궁금하신 사항은 X(트위터)로 문의해주세요',
+      onConfirm: () => {
+        window.open(
+          'https://x.com/messages/1568567420574662658-1914300317010968576',
+          '_blank',
+          'noopener,noreferrer',
+        );
+      },
+    });
+  };
 
   return (
     <div className={styles.contactContainer}>
       <div className={styles.contactTitle}>🔍 찾고있는 내용이 없나요?</div>
       <div className={styles.contactButton}>
-        <Button onClick={() => router.push('/faq/contact')}>문의하기</Button>
+        <Button onClick={handleContactButton}>문의하기</Button>
       </div>
     </div>
   );
