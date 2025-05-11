30d2a8baf0dbe1c2d120e5cd4124695e1526d579
jinsil <jinsil378900@gmail.com>
2025-05-11 14:07:37 +0900

    feat: 트위터 DM으로 바로가기

--- src/app/faq/contact/_components/GoToOfficialButton/GoToOfficialButton.tsx
+++ src/app/faq/contact/_components/GoToOfficialButton/GoToOfficialButton.tsx
@@ -1,8 +1,18 @@
+'use client';
+
 import React from 'react';
 import Button from '@/components/Button/Button';
 
 const GoToOfficialButton = () => {
-  return <Button>공식계정 바로가기</Button>;
+  const handleClick = () => {
+    window.open(
+      'https://x.com/messages/1568567420574662658-1914300317010968576',
+      '_blank',
+      'noopener,noreferrer',
+    );
+  };
+
+  return <Button onClick={handleClick}>공식계정 바로가기</Button>;
 };
 
 export default GoToOfficialButton;
