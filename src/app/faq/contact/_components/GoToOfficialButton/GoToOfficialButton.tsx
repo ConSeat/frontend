cefad7a6061b8ded52a43526a733b54b433bc54d
jinsil <jinsil378900@gmail.com>
2025-05-11 13:59:49 +0900

    feat: 공식계정 바로가기 버튼 생성

---
+++ src/app/faq/contact/_components/GoToOfficialButton/GoToOfficialButton.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '@/components/Button/Button';
+
+const GoToOfficialButton = () => {
+  return <Button>공식계정 바로가기</Button>;
+};
+
+export default GoToOfficialButton;
--- src/app/faq/contact/page.tsx
+++ src/app/faq/contact/page.tsx
@@ -1,12 +1,14 @@
 import ContactHeader from './_components/ContactHeader/ContactHeader';
 import ContactImage from './_components/ContactImage/ContactImage';
 import ContactText from './_components/ContactText/ContactText';
+import GoToOfficialButton from './_components/GoToOfficialButton/GoToOfficialButton';
+import styles from './page.module.scss';
 import React from 'react';
 import Spacing from '@/components/Spacing/Spacing';
 
 const page = () => {
   return (
-    <>
+    <div className={styles.contactContainer}>
       <ContactHeader />
 
       <Spacing size={36} />
@@ -16,7 +18,13 @@ const page = () => {
       <Spacing size={25} />
 
       <ContactImage />
-    </>
+
+      <Spacing size={50} />
+
+      <div className={styles.officialButtonContainer}>
+        <GoToOfficialButton />
+      </div>
+    </div>
   );
 };
 
