4a6ae3b1a3d0914a372db9ca4a1e20f9edb5c076
jinsil <jinsil378900@gmail.com>
2025-05-15 16:58:30 +0900

    feat: csr에서 캐싱된 토큰 보여주기

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -1,13 +1,17 @@
+let tokenCache: string | null = null;
+
 export const getAccessToken = async (): Promise<string> => {
   if (typeof window === 'undefined') {
     const { auth } = await import('@/auth');
     const session = await auth();
-    console.log('server session', session?.accessToken); // TODO: 배포 후 확인&제거
     return session?.accessToken ?? '';
   } else {
+    if (tokenCache) {
+      return tokenCache;
+    }
     const { getSession } = await import('next-auth/react');
     const session = await getSession();
-    console.log('client session', session?.accessToken); // TODO: 배포 후 확인&제거
-    return session?.accessToken ?? '';
+    tokenCache = session?.accessToken ?? '';
+    return tokenCache;
   }
 };
