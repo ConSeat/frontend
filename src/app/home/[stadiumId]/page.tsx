51ccc0eb9a2812db41a6eb3dc5975fff084ea955
jinsil <jinsil378900@gmail.com>
2025-05-27 16:05:09 +0900

    feat: 기능 선택 metadata 추가

--- src/app/home/[stadiumId]/page.tsx
+++ src/app/home/[stadiumId]/page.tsx
@@ -1,7 +1,27 @@
 import SelectMenu from './_components/SelectMenu/SelectMenu';
 import styles from './page.module.scss';
+import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 import Spacing from '@/components/Spacing/Spacing';
 import Splitter from '@/components/Splitter/Splitter';
+import { getStadiumList } from '@/apis/stadium/stadium.api';
+import { getMetadata } from '@/utils/getMetadata';
+import { findStadiumById } from '@/utils/stadium';
+
+export async function generateMetadata({ params }): Promise<Metadata> {
+  const { stadiumId } = await params;
+
+  const { data: stadiumList } = await getStadiumList();
+  const stadium = findStadiumById(stadiumList.active, Number(stadiumId));
+
+  if (!stadium) notFound();
+
+  const title = `${stadium.stadiumName} | 시야 정보 선택`;
+
+  return getMetadata({
+    title,
+  });
+}
 
 const StadiumPage = async ({ params }) => {
   const { stadiumId } = await params;
