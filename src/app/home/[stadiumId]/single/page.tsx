0cf51774fae9977267a5116c5e18665af118dbe5
jinsil <jinsil378900@gmail.com>
2025-05-27 16:06:41 +0900

    feat: 구역별 시야 metadata 추가

--- src/app/home/[stadiumId]/single/page.tsx
+++ src/app/home/[stadiumId]/single/page.tsx
@@ -1,4 +1,24 @@
 import SingleFunnel from './_components/SingleFunnel/SingleFunnel';
+import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+import { getStadiumList } from '@/apis/stadium/stadium.api';
+import { getMetadata } from '@/utils/getMetadata';
+import { findStadiumById } from '@/utils/stadium';
+
+export async function generateMetadata({ params }): Promise<Metadata> {
+  const { stadiumId } = await params;
+
+  const { data: stadiumList } = await getStadiumList();
+  const stadium = findStadiumById(stadiumList.active, Number(stadiumId));
+
+  if (!stadium) notFound();
+
+  const title = `${stadium.stadiumName} | 구역 선택 `;
+
+  return getMetadata({
+    title,
+  });
+}
 
 const SinglePage = async ({ params }) => {
   const { stadiumId } = await params;
