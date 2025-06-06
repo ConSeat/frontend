8add439dd60dadb89fb7e03ee2d3c4dd21008b14
JungHun98 <j7papa@naver.com>
2025-06-06 16:15:07 +0900

    refactor: 이미지 슬라이드 훅 변경

--- src/hooks/common/useImageSlide.ts
+++ src/hooks/common/useImageSlide.ts
@@ -1,24 +1,54 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 interface UseImageSlideProps {
-  initialIdx: number;
+  initialIdx?: number;
   totalImageNumber: number;
 }
 
-const useImageSlide = ({ initialIdx, totalImageNumber }: UseImageSlideProps) => {
-  const [imageIndex, setImageIndex] = useState(initialIdx);
+const useImageSlide = ({ initialIdx = 1, totalImageNumber }: UseImageSlideProps) => {
+  const [imageIndex, setIndex] = useState(initialIdx);
+  const [isTransitioning, setIsTransitioning] = useState(true);
+  const sliderRef = useRef<HTMLDivElement>(null);
 
   const handleClickNext = () => {
-    const nextIdx = imageIndex >= totalImageNumber - 1 ? 0 : imageIndex + 1;
-    setImageIndex(nextIdx);
+    if (imageIndex >= totalImageNumber + 1) return;
+    setIsTransitioning(true);
+    setIndex((prev) => prev + 1);
   };
 
   const handleClickPrev = () => {
-    const nextIdx = imageIndex <= 0 ? totalImageNumber - 1 : imageIndex - 1;
-    setImageIndex(nextIdx);
+    if (imageIndex <= 0) return;
+    setIsTransitioning(true);
+    setIndex((prev) => prev - 1);
   };
 
-  return { imageIndex, handleClickNext, handleClickPrev };
+  useEffect(() => {
+    const slider = sliderRef.current;
+    if (!slider) return;
+
+    const handleTransitionEnd = () => {
+      if (imageIndex === totalImageNumber + 1) {
+        setIsTransitioning(false);
+        setIndex(1);
+      }
+
+      if (imageIndex === 0) {
+        setIsTransitioning(false);
+        setIndex(totalImageNumber);
+      }
+    };
+
+    slider.addEventListener('transitionend', handleTransitionEnd);
+    return () => slider.removeEventListener('transitionend', handleTransitionEnd);
+  }, [imageIndex, totalImageNumber]);
+
+  return {
+    imageIndex,
+    isTransitioning,
+    sliderRef,
+    handleClickNext,
+    handleClickPrev,
+  };
 };
 
 export default useImageSlide;
