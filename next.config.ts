cf8250dd0e46c64ee450180f92829867991d5377
jinsil <jinsil378900@gmail.com>
2025-06-09 02:09:28 +0900

    feat: sentry 설치, 초기 설정

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { withSentryConfig } from '@sentry/nextjs';
 import withPWA from 'next-pwa';
 import runtimeCaching from 'next-pwa/cache';
 import path from 'path';
@@ -33,4 +34,11 @@ const nextConfig = {
   },
 };
 
-export default pwaConfig(nextConfig);
+export default withSentryConfig(pwaConfig(nextConfig), {
+  org: 'conseat',
+  project: 'javascript-nextjs',
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  tunnelRoute: '/monitoring',
+  disableLogger: true,
+});
