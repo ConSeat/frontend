5dc7e8713a431d6d88f862d506a56c330ea9c3d0
jinsil <jinsil378900@gmail.com>
2025-06-09 16:39:08 +0900

    feat: sentry.client 파일 제거

--- sentry.client.config.ts
+++
@@ -1,9 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 0.1,
-  sendDefaultPii: false, // 개인정보 false, 보내고 싶은 개인 정보는 /members 조회 시 setUser
-});
-
-export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -3,10 +3,11 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   integrations: [Sentry.replayIntegration()],
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
-  debug: false,
+  debug: true,
+  sendDefaultPii: false, // 개인정보 false, 보내고 싶은 개인 정보는 /members 조회 시 setUser
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
