9727c2f052e5f29ead41dcf9849a066b5258ad52
jinsil <jinsil378900@gmail.com>
2025-06-09 18:29:49 +0900

    feat: ApiRequestError에 상태, 엔드포인트, 에러코드, 전략 추가

--- src/utils/ApiRequestError.ts
+++ src/utils/ApiRequestError.ts
@@ -1,10 +1,41 @@
+import type { Method } from '@/apis/common/apiService';
+
 interface ApiRequestErrorArg {
   errorMessage: string;
+  status: number;
+  endpoint: string;
+  method: Method;
+  requestBody?: unknown;
+  errorCode?: string;
+  strategy: 'toast' | 'errorBoundary';
 }
 
 class ApiRequestError extends Error {
-  constructor({ errorMessage }: ApiRequestErrorArg) {
+  status: number;
+  endpoint: string;
+  method: Method;
+  requestBody?: unknown;
+  errorCode?: string;
+  strategy: 'toast' | 'errorBoundary';
+
+  constructor({
+    errorMessage,
+    status,
+    endpoint,
+    method,
+    requestBody,
+    errorCode,
+    strategy,
+  }: ApiRequestErrorArg) {
     super(errorMessage);
+
+    this.status = status;
+    this.endpoint = endpoint;
+    this.method = method;
+    this.requestBody = requestBody;
+    this.errorCode = errorCode;
+    this.strategy = strategy;
+    this.name = 'ApiRequestError';
   }
 }
 
