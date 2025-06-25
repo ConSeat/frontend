4b4cd74eb1891b2462259e170af26a17c661131d
jinsil <jinsil378900@gmail.com>
2025-06-25 17:38:36 +0900

    feat: 마이페이지 모달 길어질 때 스크롤 추가

--- src/app/mypage/_components/MyReviewCard/MyReviewCard.tsx
+++ src/app/mypage/_components/MyReviewCard/MyReviewCard.tsx
@@ -19,40 +19,44 @@ const MyReviewCard = ({ reviewId, closeModal }) => {
   }
 
   return (
-    <ReviewCard className={styles.myReviewCardContainer}>
-      <ReviewCard.Header>
-        <ReviewCard.UserInfo
-          profileSrc={review.writerSrc}
-          userName={review.writerNickname}
-          uploadTime={review.createdAt}
-        />
-      </ReviewCard.Header>
+    <div className={styles.myReviewCard}>
+      <div className={styles.myReviewCardScroll}>
+        <ReviewCard>
+          <ReviewCard.Header>
+            <ReviewCard.UserInfo
+              profileSrc={review.writerSrc}
+              userName={review.writerNickname}
+              uploadTime={review.createdAt}
+            />
+          </ReviewCard.Header>
 
-      <ReviewCard.ImageList>
-        {review.images.map((src, index) => (
-          <ReviewCard.ImageItem key={index + src} imageSrc={src} />
-        ))}
-      </ReviewCard.ImageList>
+          <ReviewCard.ImageList>
+            {review.images.map((src, index) => (
+              <ReviewCard.ImageItem key={index + src} imageSrc={src} />
+            ))}
+          </ReviewCard.ImageList>
 
-      <ReviewCard.ConcertTitle concertName={review.concertName} />
+          <ReviewCard.ConcertTitle concertName={review.concertName} />
 
-      <ReviewCard.ConcertDescription contents={review.contents} />
+          <ReviewCard.ConcertDescription contents={review.contents} />
 
-      <div className={styles.reviewKeywordList}>
-        <ReviewCard.KeywordList keywordArray={review.features} isPrimary={true} />
-        <ReviewCard.KeywordList keywordArray={review.obstructions} isPrimary={false} />
+          <div className={styles.reviewKeywordList}>
+            <ReviewCard.KeywordList keywordArray={review.features} isPrimary={true} />
+            <ReviewCard.KeywordList keywordArray={review.obstructions} isPrimary={false} />
+          </div>
+
+          <Splitter color="sub-gray6" />
+
+          <ReviewCard.Screening status={review.status} rejectReason={review.rejectReason} />
+
+          <Splitter color="sub-bg-black" height="12px" />
+
+          <Button className={styles.closeButton} onClick={closeModal}>
+            닫기
+          </Button>
+        </ReviewCard>
       </div>
-
-      <Splitter color="sub-gray6" />
-
-      <ReviewCard.Screening status={review.status} rejectReason={review.rejectReason} />
-
-      <Splitter color="sub-bg-black" height="12px" />
-
-      <Button className={styles.closeButton} onClick={closeModal}>
-        닫기
-      </Button>
-    </ReviewCard>
+    </div>
   );
 };
 
