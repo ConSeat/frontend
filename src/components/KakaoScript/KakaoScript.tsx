5d8d09d50d1d6a79e02b26ddbc0c9a5aac8bf810
chlwlstlf <jinsil378900@gmail.com>
2025-09-20 23:20:10 +0900

    fix: onReady로 변경

--- src/components/KakaoScript/KakaoScript.tsx
+++ src/components/KakaoScript/KakaoScript.tsx
@@ -29,7 +29,15 @@ const KakaoScript = () => {
     <Script
       src="https://developers.kakao.com/sdk/js/kakao.js"
       strategy="afterInteractive"
-      onLoad={onLoad}
+      // onLoad={onLoad} 에러떠서 일단 주석 처리
+      onReady={() => {
+        // 스크립트가 실행 완료된 시점이므로 window.Kakao가 존재합니다.
+        if (!window.Kakao?.isInitialized()) {
+          // 카카오 개발자 콘솔의 'JavaScript 키'를 사용합니다.
+          // https://developers.kakao.com/docs/latest/en/javascript/getting-started
+          window.Kakao.init(PUBLIC_ENV.kakaoApiKey!);
+        }
+      }}
     />
   );
 };
