ca2099f5bd1c019eba9336ab735b1dcceed0a5ee
chlwlstlf <jinsil378900@gmail.com>
2025-09-20 23:50:20 +0900

    fix: type error 해결

--- src/components/KakaoScript/KakaoScript.tsx
+++ src/components/KakaoScript/KakaoScript.tsx
@@ -4,32 +4,10 @@ import Script from 'next/script';
 import { PUBLIC_ENV } from '@/config/env';
 
 const KakaoScript = () => {
-  const onLoad = () => {
-    try {
-      const Kakao = (window as any).Kakao;
-      if (!Kakao) {
-        console.warn('Kakao SDK not loaded yet');
-        return;
-      }
-
-      if (!Kakao.isInitialized()) {
-        if (!PUBLIC_ENV.kakaoApiKey) {
-          console.error('Kakao API key is missing');
-          return;
-        }
-        Kakao.init(PUBLIC_ENV.kakaoApiKey);
-        console.info('Kakao SDK initialized');
-      }
-    } catch (err) {
-      console.error('Kakao initialization error:', err);
-    }
-  };
-
   return (
     <Script
       src="https://developers.kakao.com/sdk/js/kakao.js"
       strategy="afterInteractive"
-      // onLoad={onLoad} 에러떠서 일단 주석 처리
       onReady={() => {
         // 스크립트가 실행 완료된 시점이므로 window.Kakao가 존재합니다.
         if (!window.Kakao?.isInitialized()) {
